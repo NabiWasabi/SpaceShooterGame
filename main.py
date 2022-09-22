f654b59106329bde4ed57aa85870214c5b30e004
NabiWasabi <nabiehs@gmail.com>
2022-09-21 21:53:34 -0700

    Lives Feature

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ class Ship:
         return self.ship_img.get_height()
 
 class Player(Ship):
-    def __int__(self, x, y, health=100):
-        super().__init__(self, x, y, health)
+    def __init__(self, x, y, health=100):
+        super().__init__( x, y, health)
         self.ship_img = YELLOW_SPACE_SHIP
         self.laser_img = YELLOW_LASER
         self.mask = pygame.mask.from_surface(self.ship_img)
@@ -59,7 +59,7 @@ class Enemy(Ship):
                 "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
     }
 
-    def __int__(self, x, y, color, health=100):
+    def __init__(self, x, y, color, health=100):
         super().__init__(x,y,health)
         self.ship_img, self.laser_img = self.COLOR_MAP[color]
         self.mask = pygame.mask.from_surface(self.ship_img)
@@ -72,9 +72,10 @@ class Enemy(Ship):
 def main():
     run = True
     FPS = 60
-    level= 1
+    level = 1
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+    lost_font = pygame.font.SysFont("comicsans", 60)
 
     enemies = []
     wave_length = 5
@@ -86,10 +87,12 @@ def main():
 
     clock = pygame.time.Clock()
 
+    lost = False
+
     def redraw_window():
         WIN.blit(BG, (0,0))
         # Draw Text
-        lives_label = main_font.render(f"Lives: {lives}", 1, (255,0,0))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
         level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
 
         WIN.blit(lives_label, (10,10))
@@ -100,14 +103,21 @@ def main():
 
         player.draw(WIN)
 
+        if lost:
+            lost_label = lost_font.render("You Lost!", 1, (255,255,255))
+            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
+
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
 
+        if lives <= 0 or player.health <= 0:
+            lost = True
+
         if len(enemies) == 0:
             level += 1
-            wave_length =+ 5
+            wave_length += 5
             for i in range(wave_length):
                 enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
                 enemies.append(enemy)
@@ -126,8 +136,12 @@ def main():
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: #down
             player.y += player_vel
 
-        for enemy in enemies:
+        for enemy in enemies[:]:
             enemy.move(enemy_vel)
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+
         redraw_window()
 
 main()
